6f1162a68963772673817384a1438edc7e95ee7a
Jim Remsik <jim@beflagrant.com>
2023-12-22 16:55:43 -0600

    Use fewer ambiguous values

--- src/_components/shared/button.rb
+++ src/_components/shared/button.rb
@@ -3,11 +3,11 @@ class Shared::Button < Bridgetown::Component
     @text, @href, @type = text, href, type
     case @type
     when :secondary
-      @classname = "text-medium-grey border border-medium-grey text-base rounded-[2px] px-[15px] py-3 uppercase hover:text-lime-green hover:border-lime-green hover:underline hover:opacity-70 active:no-underline block min-h-[50px] min-w-[190px] w-fit text-center"
+      @classname = "text-medium-grey border border-medium-grey text-meta rounded-sm px-[15px] py-3 hover:text-lime-green hover:border-lime-green hover:underline hover:opacity-70 active:no-underline block min-h-[50px] min-w-[190px] w-fit text-center"
     when :tertiary
-      @classname = "text-lime-green border border-lime-green text-base rounded-[2px] px-5 uppercase hover:text-lime-green hover:border-lime-green hover:underline hover:opacity-70 active:no-underline block min-h-[50px] min-w-[190px] w-fit text-center py-3"
+      @classname = "text-lime-green border border-lime-green text-meta rounded-sm px-5 hover:text-lime-green hover:border-lime-green hover:underline hover:opacity-70 active:no-underline block min-h-[50px] min-w-[190px] w-fit text-center py-3"
     else
-      @classname = "bg-lime-green text-base rounded-sm px-[15px] py-3 uppercase hover:underline hover:opacity-70 active:no-underline  block min-h-[50px] min-w-[190px] w-fit text-center"
+      @classname = "bg-lime-green text-black text-meta rounded-sm px-[15px] py-3 hover:underline hover:opacity-70 active:no-underline  block min-h-[50px] min-w-[190px] w-fit text-center"
     end
   end
 end
