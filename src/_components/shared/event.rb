61ce5637ea9b5d92b1c0c1dca8eab30221232fd0
SaVance <savance@savances-mbp.home>
2024-01-08 09:37:00 -0600

    WIP for componentizing events for landing page

--- src/_components/shared/event.rb
+++ src/_components/shared/event.rb
@@ -1,12 +1,16 @@
 class Shared::Event < Bridgetown::Component
-  def initialize(event:, title:, contents:, collection:, cluster:, align:)
-    @event, @title, @contents, @collection, @cluster, @align = text = event, title, collection, contents, cluster, align
-
+  def initialize(event:, align:, breakall:, cluster:)
+    @event, @align, @cluster, @breakall = event, align, cluster, breakall
     case @align
     when :right
       @classname = 'sm:text-right'
     when :left
       @classname = 'sm:text-left'
+    end
+
+    case @breakall
+    when :true
+      @breakAllClass = 'break-all'
     end
   end
 end
