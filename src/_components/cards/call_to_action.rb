4a1d243d28dd6cfe7161a231fde9099d667616a9
SaVance <savance@savances-mbp.home>
2024-01-15 18:31:45 -0600

    add ellipsis logic for event, cta and journal content summary

--- src/_components/cards/call_to_action.rb
+++ src/_components/cards/call_to_action.rb
@@ -1,6 +1,6 @@
 class Cards::CallToAction < Bridgetown::Component
   def initialize(post:)
-    @content = post.content_summary(length: 150)
+    @content = post.content_summary(length: 125)
     @cta     = post.data
     @link    = post.relative_url
   end
--- src/_components/cards/event.rb
+++ src/_components/cards/event.rb
@@ -1,6 +1,6 @@
 class Cards::Event < Bridgetown::Component
   def initialize(post:)
-    @content = post.content_summary(length: 150)
+    @content = post.content_summary(length: 125)
     @event = post.data
     @link = post.relative_url
   end
