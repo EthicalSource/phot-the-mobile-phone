4f0b12060bf6ce708389e835b9ff70b56dce8d30
Jim Remsik <jim@beflagrant.com>
2024-01-19 11:10:33 -0600

    Draw correct explore more related to journals

--- plugins/journal.rb
+++ plugins/journal.rb
@@ -1,30 +1,33 @@
 require 'pry'
 class Journal < Bridgetown::Model::Base
   def resources_to_explore
-    five_random_journals.
-      push(cta_resource).
-      compact
+    output = other_journals
+    output = output.insert(2,prompt_resource) if prompt_resource
+    output = output.insert(4,cta_resource) if cta_resource
+    output.compact
   end
 
   private
-
-  def five_random_journals
-    other_journals.
-      sample(5)
-  end
 
   def cta_resource
     site_collections.
       ctas.
       resources.
-      find { |cta| cta.data.clusters == clusters }
+      find { |cta| cta.data.clusters.downcase == clusters.downcase }
+  end
+
+  def prompt_resource
+    site_collections.
+      prompts.
+      resources.
+      find { |prompt| prompt.data.clusters.downcase == clusters.downcase }
   end
 
   def other_journals
     site_collections.
       journals.
       resources.
-      reject { |journal| journal.data.title == title }
+      reject { |journal| journal.data.title.downcase == title.downcase }
   end
 
   def site_collections
