b001c5cab7577ae0a9a881d8318c779af06a307c
Jim Remsik <jim@beflagrant.com>
2024-01-19 08:21:53 -0600

    Handle disabling and enabling mobile swiper buttons

--- frontend/javascript/controllers/carousel_controller.js
+++ frontend/javascript/controllers/carousel_controller.js
@@ -23,8 +23,18 @@ export default class extends Controller {
                 prevEl: this.prevTarget,
             },
         });
-        document.querySelector('.mobile > [data-carousel-target="prev"]').addEventListener('click', () => this.swiper.slidePrev());
-        document.querySelector('.mobile > [data-carousel-target="next"]').addEventListener('click', () => this.swiper.slideNext());
+        document.querySelector('.mobile > [data-carousel-target="prev"]').addEventListener('click', () => {
+            this.swiper.slidePrev();
+            this.handleSwiperPrev();
+            this.handleSwiperNext();
+        });
+        document.querySelector('.mobile > [data-carousel-target="next"]').addEventListener('click', () => {
+            this.swiper.slideNext();
+            this.handleSwiperNext();
+            this.handleSwiperPrev();
+        });
+        this.handleSwiperPrev();
+        this.handleSwiperNext();
     }
 
     disconnect() {
@@ -33,4 +43,21 @@ export default class extends Controller {
             this.swiper = undefined;
         }
     }
+
+    handleSwiperPrev() {
+        if (this.prevTarget.disabled) {
+            document.querySelector('.mobile > [data-carousel-target="prev"]').classList.add('swiper-button-disabled');
+        } else {
+            document.querySelector('.mobile > [data-carousel-target="prev"]').classList.remove('swiper-button-disabled');
+        }
+    }
+
+    handleSwiperNext() {
+        if (this.nextTarget.disabled) {
+            document.querySelector('.mobile > [data-carousel-target="next"]').classList.add('swiper-button-disabled');
+        } else {
+            document.querySelector('.mobile > [data-carousel-target="next"]').classList.remove('swiper-button-disabled');
+        }
+    }
+
 }
