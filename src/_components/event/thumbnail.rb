449f718a7ba6f733a0dfbc14aac6de82f40e3670
SaVance <savance@savances-mbp.home>
2024-01-24 14:56:05 -0600

    Use the new images from the previous commit

--- src/_components/event/thumbnail.rb
+++ src/_components/event/thumbnail.rb
@@ -1,7 +1,7 @@
 class Event::Thumbnail < Bridgetown::Component
   def initialize(event:)
     @event = event
-    @image = event.data.media&.thumbnail
-    @alt = event.data.media&.alt
+    @image = event.data.homepage_thumbnail&.image
+    @alt = event.data.homepage_thumbnail&.alt
   end
 end
