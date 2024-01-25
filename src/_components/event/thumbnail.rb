c423061417f2870d8cfc5eff4bf3f8a144515f43
Jim Remsik <jim@beflagrant.com>
2024-01-25 13:48:04 -0600

    Fallback to event image

--- src/_components/event/thumbnail.rb
+++ src/_components/event/thumbnail.rb
@@ -1,7 +1,7 @@
 class Event::Thumbnail < Bridgetown::Component
   def initialize(event:)
     @event = event
-    @image = event.data.homepage_thumbnail&.image
+    @image = event.data.homepage_thumbnail&.image || event.data.media&.thumbnail
     @alt = event.data.homepage_thumbnail&.alt
   end
 end
