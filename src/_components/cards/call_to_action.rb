0a1cc791119b94a41b4d0eaaea7dab12dfab07a3
Jim Remsik <jim@beflagrant.com>
2024-01-25 22:38:18 -0600

    Apply collection entry image/alt tags on events/resources cards

--- src/_components/cards/call_to_action.rb
+++ src/_components/cards/call_to_action.rb
@@ -3,5 +3,14 @@ class Cards::CallToAction < Bridgetown::Component
     @content = post.summary_summary(length: 175)
     @cta     = post.data
     @link    = post.relative_url
+
+    photo = post.data.cluster_thumbnail
+    if photo && !photo.src&.blank?
+      @image = photo.src
+      @alt = photo.alt
+    else
+      @image = post.data.media&.thumbnail
+      @alt = post.data.media&.alt
+    end
   end
 end
--- src/_components/cards/event.rb
+++ src/_components/cards/event.rb
@@ -3,5 +3,14 @@ class Cards::Event < Bridgetown::Component
     @content = post.summary_summary(length: 125)
     @event = post.data
     @link = post.relative_url
+
+    photo = post.data.cluster_thumbnail
+    if photo && !photo.src&.blank?
+      @image = photo.src
+      @alt = photo.alt
+    else
+      @image = post.data.media&.thumbnail
+      @alt = post.data.media&.alt
+    end
   end
 end
